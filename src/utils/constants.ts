c7deb0c47a5f277e1a917ee98f38e79d05c15de3
Joshua Sum <joshsum@umich.edu>
2024-07-31 16:01:19 -0400

    Update constants to mainnet

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { PublicKey } from '@solana/web3.js';
 
-export const PROGRAM_ID = new PublicKey("ArphYhAXHyH1cPfXFe22iBur51QuB2UkFAdToUBjhDUp")
-export const DELEGATED_TOKEN_MINT_ID = new PublicKey("8doaCnhsE5Ppi5dNHhAZwTQtYS4gVy28CWRcfmMuRKPk")
-export const PDA_SEED: string = "endo_avs"
+export const PROGRAM_ID = new PublicKey("endoLNCKTqDn8gSVnN2hDdpgACUPWHZTwoYnnMybpAT");
+export const DELEGATED_TOKEN_MINT_ID = new PublicKey("sSo14endRuUbvQaJS3dq36Q829a3A6BEfoeeRGJywEh");
+export const PDA_SEED: string = "endo_avs";
 export const METADATA_PROGRAM_ID = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
