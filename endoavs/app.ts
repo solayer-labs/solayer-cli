86977d96367cc2ef04c956adf84c54f9e27317ac
Suhail Kakar <suhail.zahed@gmail.com>
2024-08-13 20:58:54 +0530

    feat: add prompts

--- endoavs/app.ts
+++ endoavs/app.ts
@@ -1,81 +1,254 @@
-import { Command, Option } from "commander";
 import { createAvs } from "./actions/create";
 import { updateAvs } from "./actions/updateAvs";
 import { delegate, undelegate } from "./actions/delegate";
 import { transferAuthority } from "./actions/transferAuthority";
 import { updateMetadata } from "./actions/updateMetadata";
-import { execSync } from "child_process";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import prompts from "prompts";
+import printLogo from "../utils/logo";
+import checkSolanaConfig from "../utils/solana-config";
 
-const program = new Command();
+const main = async () => {
+  printLogo();
+  const { rpcUrl, keypairPath } = await checkSolanaConfig();
 
-let rpcUrl = "https://api.mainnet-beta.solana.com";
-let keypairPath = "~/.config/solana/id.json";
-try {
-  const solanaConfig = execSync("solana config get", {
-    stdio: ["ignore", "pipe", "ignore"],
-  }).toString();
+  const argv = yargs(hideBin(process.argv))
+    .option("action", {
+      alias: "a",
+      describe: "Action to perform",
+      choices: [
+        "create",
+        "delegate",
+        "undelegate",
+        "transferAuthority",
+        "updateAvs",
+        "updateMetadata",
+      ],
+    })
+    .option("avsName", {
+      describe: "Name of the AVS",
+      type: "string",
+    })
+    .option("avsTokenMintKeyPairPath", {
+      describe: "Path to AVS token mint keypair",
+      type: "string",
+    })
+    .option("numberOfSOL", {
+      describe: "Number of SOL to delegate or undelegate",
+      type: "number",
+    })
+    .option("endoAvsAddress", {
+      describe: "Address of the endoAvs",
+      type: "string",
+    })
+    .option("newAuthorityAddr", {
+      describe: "New authority address",
+      type: "string",
+    })
+    .option("newName", {
+      describe: "New name for the AVS",
+      type: "string",
+    })
+    .option("newUrl", {
+      describe: "New URL for the AVS",
+      type: "string",
+    })
+    .option("name", {
+      describe: "Name for the metadata",
+      type: "string",
+    })
+    .option("symbol", {
+      describe: "Symbol for the metadata",
+      type: "string",
+    })
+    .option("uri", {
+      describe: "URI for the metadata",
+      type: "string",
+    }).argv;
 
-  const rpcUrlMatch = solanaConfig.match(/RPC URL:\s*(.*)/);
-  const keypairPathMatch = solanaConfig.match(/Keypair Path:\s*(.*)/);
+  const action =
+    argv.action ||
+    (
+      await prompts({
+        type: "select",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+          { title: "Create AVS", value: "create" },
+          { title: "Delegate SOL", value: "delegate" },
+          { title: "Undelegate SOL", value: "undelegate" },
+          { title: "Transfer Authority", value: "transferAuthority" },
+          { title: "Update AVS", value: "updateAvs" },
+          { title: "Update Metadata", value: "updateMetadata" },
+        ],
+      })
+    ).action;
 
-  if (rpcUrlMatch) {
-    rpcUrl = rpcUrlMatch[1].trim();
+  switch (action) {
+    case "create":
+      const avsName =
+        argv.avsName ||
+        (
+          await prompts({
+            type: "text",
+            name: "avsName",
+            message: "Enter the name of the AVS:",
+            validate: (value) => value.length > 0 || "AVS name is required",
+          })
+        ).avsName;
+
+      const avsTokenMintKeyPairPath =
+        argv.avsTokenMintKeyPairPath ||
+        (
+          await prompts({
+            type: "text",
+            name: "avsTokenMintKeyPairPath",
+            message: "Enter the path to the AVS token mint keypair:",
+            validate: (value) =>
+              value.length > 0 || "Token mint keypair path is required",
+          })
+        ).avsTokenMintKeyPairPath;
+
+      await createAvs(rpcUrl, keypairPath, avsName, avsTokenMintKeyPairPath);
+      break;
+
+    case "delegate":
+    case "undelegate":
+      const numberOfSOL =
+        argv.numberOfSOL ||
+        (
+          await prompts({
+            type: "number",
+            name: "numberOfSOL",
+            message: `Enter the number of SOL to ${action}:`,
+            validate: (value) => value > 0 || "Amount must be greater than 0",
+          })
+        ).numberOfSOL;
+
+      const endoAvsAddress =
+        argv.endoAvsAddress ||
+        (
+          await prompts({
+            type: "text",
+            name: "endoAvsAddress",
+            message: "Enter the endoAvs address:",
+            validate: (value) =>
+              value.length > 0 || "endoAvs address is required",
+          })
+        ).endoAvsAddress;
+
+      if (action === "delegate") {
+        await delegate(rpcUrl, keypairPath, numberOfSOL, endoAvsAddress);
+      } else {
+        await undelegate(rpcUrl, keypairPath, numberOfSOL, endoAvsAddress);
+      }
+      break;
+
+    case "transferAuthority":
+      const newAuthorityAddr =
+        argv.newAuthorityAddr ||
+        (
+          await prompts({
+            type: "text",
+            name: "newAuthorityAddr",
+            message: "Enter the new authority address:",
+            validate: (value) =>
+              value.length > 0 || "New authority address is required",
+          })
+        ).newAuthorityAddr;
+
+      await transferAuthority(
+        rpcUrl,
+        keypairPath,
+        newAuthorityAddr,
+        argv.endoAvsAddress
+      );
+      break;
+
+    case "updateAvs":
+      const newName =
+        argv.newName ||
+        (
+          await prompts({
+            type: "text",
+            name: "newName",
+            message: "Enter the new name for the AVS:",
+            validate: (value) => value.length > 0 || "New AVS name is required",
+          })
+        ).newName;
+
+      const newUrl =
+        argv.newUrl ||
+        (
+          await prompts({
+            type: "text",
+            name: "newUrl",
+            message: "Enter the new URL for the AVS:",
+            validate: (value) => value.length > 0 || "New AVS URL is required",
+          })
+        ).newUrl;
+
+      await updateAvs(
+        rpcUrl,
+        keypairPath,
+        newName,
+        newUrl,
+        argv.endoAvsAddress
+      );
+      break;
+
+    case "updateMetadata":
+      const name =
+        argv.name ||
+        (
+          await prompts({
+            type: "text",
+            name: "name",
+            message: "Enter the name for the metadata:",
+            validate: (value) => value.length > 0 || "Name is required",
+          })
+        ).name;
+
+      const symbol =
+        argv.symbol ||
+        (
+          await prompts({
+            type: "text",
+            name: "symbol",
+            message: "Enter the symbol for the metadata:",
+            validate: (value) => value.length > 0 || "Symbol is required",
+          })
+        ).symbol;
+
+      const uri =
+        argv.uri ||
+        (
+          await prompts({
+            type: "text",
+            name: "uri",
+            message: "Enter the URI for the metadata:",
+            validate: (value) => value.length > 0 || "URI is required",
+          })
+        ).uri;
+
+      await updateMetadata(
+        rpcUrl,
+        keypairPath,
+        name,
+        symbol,
+        uri,
+        argv.endoAvsAddress
+      );
+      break;
+
+    default:
+      console.log("Invalid option selected.");
+      break;
   }
+};
 
-  if (keypairPathMatch) {
-    keypairPath = keypairPathMatch[1].trim();
-  }
-} catch (error) {}
-
-program
-  .version("0.0.1")
-  .description("CLI for interacting with the endoavs program");
-program
-  .addOption(
-    new Option("-k, --keypair <path-to-wallet-json-file>").default(keypairPath)
-  )
-  .addOption(new Option("-u, --url <url>").default(rpcUrl));
-
-program
-  .command("create <avsName> <avsTokenMintKeyPairPath>")
-  .description("Create a new endoAvs")
-  .action(async (avsName, avsTokenMintKeyPairPath) => {
-    await createAvs(rpcUrl, keypairPath, avsName, avsTokenMintKeyPairPath);
-  });
-
-program
-  .command("delegate <numberOfSOL> <endoAvsAddress>")
-  .description("Delegate SOL to endoAvs")
-  .action(async (numberOfSOL, endoAvsAddress) => {
-    await delegate(rpcUrl, keypairPath, numberOfSOL, endoAvsAddress);
-  });
-
-program
-  .command("undelegate <numberOfSOL> <endoAvsAddress>")
-  .description("Undelegate SOL to endoAvs")
-  .action(async (numberOfSOL, endoAvsAddress) => {
-    await undelegate(rpcUrl, keypairPath, numberOfSOL, endoAvsAddress);
-  });
-
-program
-  .command("transferAuthority <newAuthorityAddr> <endoAvsAddress>")
-  .description("Transfer authority to a new address")
-  .action(async (newAuthorityAddr, endoAvsAddress) => {
-    await transferAuthority(rpcUrl, keypairPath, newAuthorityAddr, endoAvsAddress);
-  });
-
-program
-  .command("updateAvs <newName> <newUrl> <endoAvsAddress>")
-  .description("Update an existing endoAvs")
-  .action(async (newName, newUrl, endoAvsAddress) => {
-    await updateAvs(rpcUrl, keypairPath, newName, newUrl, endoAvsAddress);
-  });
-
-program
-  .command("updateMetadata <name> <symbol> <uri> <endoAvsAddress>")
-  .description("Update the metadata of the endoAvs token")
-  .action(async (name, symbol, uri, endoAvsAddress) => {
-    await updateMetadata(rpcUrl, keypairPath, name, symbol, uri, endoAvsAddress);
-  });
-
-program.parse(process.argv);
+main().catch((error) => {
+  console.error("An error occurred:", error);
+  process.exit(1);
+});
